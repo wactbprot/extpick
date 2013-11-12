8a8d5f92c88448af0ddeddd4672d5b8144f0b6a2
wactbprot <thsteinbock@web.de>
2013-11-12 19:06:47 +0100

    return to where

--- _attachments/js/vl.ko.js
+++ _attachments/js/vl.ko.js
@@ -1,14 +1,35 @@
 // Here's my data model
 var ViewModel = function(_standard, _type, _year, _sign) {
-    this.standard = ko.observable(_standard);
-    this.type = ko.observable(_type);
-    this.year = ko.observable(_year);
-    this.sign = ko.observable(_sign);
+    var that = this;
+
+    that.standard = ko.observable(_standard);
+    that.type = ko.observable(_type);
+    that.year = ko.observable(_year);
+    that.sign = ko.observable(_sign);
  
-    this.docsThatMatch = ko.computed(function() {
-					 
-				    return this.standard() + " " + this.sign();
-    }, this);
+    that.docsThatMatch = ko.computed(
+	function() {
+	    
+	    // couchcall
+	    // success : function(data){
+	    var req ={};
+	    
+	    req.url = "_list/overview/docs";
+            req.data = {key: "\"" + that.year() + "_"+
+			that.standard() + "_"+
+			that.type() + "_"+
+			that.sign()+ "\""};
+	    
+            req.success = function(html){
+		// ?????????
+
+	return html;
+            };	
+            $.ajax(req);
+	    
+	}, that);
+    
+  
 };
  
 ko.applyBindings(new ViewModel("CE3", "VG", "2013", "0070_0001"));
