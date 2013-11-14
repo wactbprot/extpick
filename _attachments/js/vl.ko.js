55714430237c16c4e29cf52df1c138b2316ad98d
wactbprot <thsteinbock@web.de>
2013-11-14 15:44:56 +0100

    images included

--- _attachments/js/vl.ko.js
+++ _attachments/js/vl.ko.js
@@ -1,36 +1,35 @@
-// Here's my data model
 var ViewModel = function(_standard, _type, _year, _sign) {
     var that = this;
-
+    
     that.standard = ko.observable(_standard);
     that.type = ko.observable(_type);
     that.year = ko.observable(_year);
     that.sign = ko.observable(_sign);
- 
-    that.docsThatMatch = ko.computed(
-	function() {
-	    
-	    // couchcall
-	    // success : function(data){
-	    var req ={};
-	    
-	    req.url = "_list/overview/docs";
-            req.data = {key: "\"" + that.year() + "_"+
-			that.standard() + "_"+
-			that.type() + "_"+
-			that.sign()+ "\""};
-	    
-            req.success = function(html){
-		// ?????????
-
-	return html;
-            };	
-            $.ajax(req);
-	    
-	}, that);
+    
+    that.docsThatMatch = ko.observable();
+    
+    ko.dependentObservable(function() {
+			       // couchcall
+			       // success : function(data){
+			       var req ={};
+			       
+			       req.url = "_list/overview/docs";
+			       req.data = {startkey: "\"" + that.year() + "_"+
+					   that.standard() + "_"+
+					   that.type() + "_"+
+					   that.sign()+ "\"",
+					   endkey:"\"" + that.year() + "_"+
+					   that.standard() + "_"+
+					   that.type() + "_"+
+					   that.sign()+"zzzzzzzzz"+ "\""};
+			       
+			       req.success  =   that.docsThatMatch;
+			       $.ajax(req);
+			       
+			   }, that);
     
   
 };
  
-ko.applyBindings(new ViewModel("CE3", "VG", "2013", "0070_0001"));
+ko.applyBindings(new ViewModel("CE3", "VG", "2013", "0070"));
 
--- lists/overview.js
+++ lists/overview.js
@@ -5,28 +5,48 @@ function(head, req) {
 
     epbody = this.templates.extPickBody,
     r,
+    gotNoData = true,
     i   = 0,
     cnt = 0,
     bo  = {};
 
     start(share.startHtml);
 
-    while(r = getRow()) {
-	var rv = r.value;
-	var so = [];
-	if(rv._attachments){
-	    
-	    for(i in rv._attachments){
-		var rvai = rv._attachments[i];
+
+
+        while(r = getRow()) {
+            
+	    var rv = r.value;
+            var soPng = [];
+            var soXls = [];
+            if(rv._attachments){
+		gotNoData = false;
+                for(i in rv._attachments){
+                    var rvai = rv._attachments[i];
+		    if(rvai.content_type == "image/png"){
+			soPng.push({filename:i,
+				    content_type:rvai.content_type,
+				    length:rvai.length
+				   });
+		    }else{
+			soXls.push({filename:i,
+				    content_type:rvai.content_type,
+				    length:rvai.length
+				   });
 		
-		so.push({filename:i,
-		  content_type:rvai.content_type,
-			 length:rvai.length
-			});
-	    }
-	    rv.so = so;
-	    send(mustache.to_html(epbody,rv ));
-	}// if _at..
-        cnt++;
-    }//while
+		    }
+                }
+                rv.soPng = soPng;
+		rv.soXls = soXls;
+		
+                send(mustache.to_html(epbody,rv ));
+		cnt++;
+            }
+	}//while
+    
+    if(gotNoData){
+	send("<h2>nothing on database</h2>");
+    }// if _at..
+    
+    
 }
